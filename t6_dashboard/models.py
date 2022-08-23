b504daaf392dbb8d5ed8d968456f8c4bf770dc1b
kevinusmayadhyw <57991938+kevinusmayadhyw@users.noreply.github.com>
2022-08-23 19:16:55 +0700

    fix models and view

--- t6_dashboard/models.py
+++ t6_dashboard/models.py
@@ -7,3 +7,15 @@ class topUsers(models.Model):
     point = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class points(models.Model):
+    point = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class division(models.Model):
+    name = models.CharField(max_length=255)
+    department = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- t6_dashboard/views.py
+++ t6_dashboard/views.py
@@ -1,32 +1,101 @@
 from django.shortcuts import render
+from django.db.models import Sum
 from .models import topUsers
+from .models import points
+from .models import division
 
 def index(request):
     deleteAll(topUsers)
+    deleteAllPoint(points)
+    deleteAllDivision(division)
 
-    user_list = dataDummy(topUsers)[:5]
-    
+    user_list = dataDummy(topUsers)
+    point_list = dataDummyPoint(points).order_by('created_at')[:2]
+    point_sum = topUsers.objects.aggregate(Sum('point')).get('point__sum')
+    target = 200
+    percent = point_sum/target * 100
+    division_list = dataDummyDivision(division)
+    count_department = division_list.all().values('department').distinct().count()
 
-    context = {'user_list': user_list}
+    context = {'user_list': user_list, 'point_list' : point_list, 'point_sum' : point_sum, 'percent' : percent, 'target' : target, 'division_list' : division_list,'count_department' : count_department}
 
     return render(request, 't6_dashboard/index.html', context)
 
 def deleteAll(topUsers):
     topUsers.objects.all().delete()
 
+def deleteAllPoint(points):
+    points.objects.all().delete()
+
+def deleteAllDivision(division):
+    division.objects.all().delete()
+
+def dataDummyDivision(division):
+    div = division(name = "Area Marketing division 1", department = "Marketing")
+    div.save()
+
+    div = division(name = "Area Marketing division 2", department = "Marketing")
+    div.save()
+
+    div = division(name = "Area Akuntasi division 1", department = "Akuntasi")
+    div.save()
+
+    div = division(name = "Area Akuntasi division 2", department = "Akuntasi")
+    div.save()
+
+    div = division(name = "Data dummy", department = "Data dummy")
+    div.save()
+
+    div = division(name = "Data dummy 1", department = "Data dummy 1")
+    div.save()
+
+    div = division(name = "Data dummy 2", department = "Data dummy 2")
+    div.save()
+
+    div = division(name = "Data dummy 3", department = "Data dummy 3")
+    div.save()
+
+    div = division(name = "Data dummy 4", department = "Data dummy 4")
+    div.save()
+
+    return division.objects.all()
+
+
+def dataDummyPoint(points):
+    point = points(point=1000)
+    point.save()
+
+    point = points(point=1500)
+    point.save()
+
+    return points.objects.all()
+
 def dataDummy(topUsers):
-    user = topUsers(fullname = "Dhani Iskandar", department = "Marketing", division = "Area Marketing division", point=128)
+    user = topUsers(fullname = "Dhani Iskandar", department = "Marketing", division = "Area Marketing division", point=30)
     user.save()
-    user = topUsers(fullname = "Arief Key", department = "Marketing", division = "Area Marketing division", point=120)
-    user.save()
-
-    user = topUsers(fullname = "Sabiru muda", department = "Marketing", division = "Area Marketing division", point=125)
+    
+    user = topUsers(fullname = "Arief Key", department = "Marketing", division = "Area Marketing division", point=5)
     user.save()
 
-    user = topUsers(fullname = "Paday hu", department = "Akuntasi", division = "Area Akuntasi division", point=75)
+    user = topUsers(fullname = "Sabiru muda", department = "Marketing", division = "Area Marketing division", point=10)
     user.save()
 
-    user = topUsers(fullname = "Q Muhid", department = "Akuntasi", division = "Area Akuntasi division", point=50)
+    user = topUsers(fullname = "Paday hu", department = "Akuntasi", division = "Area Akuntasi division", point=15)
+    user.save()
+
+    user = topUsers(fullname = "Q Muhid", department = "Akuntasi", division = "Area Akuntasi division", point=20)
+    user.save()
+
+    user = topUsers(fullname = "Data dummy", department = "Data dummy", division = "Data dummy", point=20)
+    user.save()
+
+    user = topUsers(fullname = "Data dummy", department = "Data dummy", division = "Data dummy", point=0)
+    user.save()
+
+    user = topUsers(fullname = "Data dummy", department = "Data dummy", division = "Data dummy", point=0)
+    user.save()
+
+    user = topUsers(fullname = "Data dummy", department = "Data dummy", division = "Data dummy", point=0)
     user.save()
 
     user = topUsers(fullname = "Data dummy", department = "Data dummy", division = "Data dummy", point=0)
